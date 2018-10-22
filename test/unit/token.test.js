0a57188b32dc4ab79b03a56a544fea24ba5acd62
Sophie Graham <sophiergraham1@gmail.com>
2018-10-22 11:42:30 -0700

    passing decodes a token with payload

--- test/unit/token.test.js
+++ test/unit/token.test.js
@@ -5,4 +5,10 @@ describe('tokenizer', () => {
         const token = tokenize({ name: 'paul' });
         expect(token).toEqual(expect.any(String));
     });
+
+    it('decodes a token with payload', () => {
+        const token = tokenize({ name: 'paul' });
+        const decodedToken = untokenize(token);
+        expect(decodedToken).toEqual({ name: 'paul' });
+    });
 });
