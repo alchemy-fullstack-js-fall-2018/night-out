10581c33e9b44f84d23364ca286d4ee648c5af63
Ryan Luras <ryanluras@gmail.com>
2018-10-22 16:08:42 -0700

    Ryanbranch (#6)
    * evening unit test passing, working on mock data
    
    * mocking data for end to end tests

--- test/e2e/mockData.js
+++ test/e2e/mockData.js
@@ -3,20 +3,7 @@ require('../../lib/util/connect')();
 const { dropCollection } = require('../util/db');
 const request = require('supertest');
 const app = require('../../lib/app');
-const { Types } = require('mongoose');
-
-const getErrors = (validation, numberExpected) => {
-    expect(validation).toBeDefined();
-    const errors = validation.errors;
-    expect(Object.keys(errors)).toHaveLength(numberExpected);
-    return errors;
-};
-
-module.exports = {
-    getErrors
-};
-
-
+// const { Types } = require('mongoose');
 
 beforeEach(() => {
     return dropCollection('users');
@@ -29,8 +16,8 @@ beforeEach(() => {
 });
 
 let createdUsers;
-let createdEvenings;
-let createdLogs;
+// let createdEvenings;
+// let createdLogs;
 
 let users = [
     {
@@ -52,3 +39,39 @@ let users = [
         initialPreferences: ['pref1', 'pref2', 'pref3', 'pref4', 'pref5', 'pref6', 'pref7', 'pref8', 'pref9']
     }
 ];
+
+// let logs = [
+//     {
+//         place_id: 'ChIJIfBAsjeuEmsRdgu9Pl1Ps48',
+//         user_id: Types.ObjectId(),
+//         rating: 'liked',
+//         tags: ['cheap', 'quiet', 'italian'],
+//         price: 1
+//     },
+//     {
+//         place_id: 'ChIJIfBAsjeuEmsRdgu9Pl1Ps73',
+//         user_id: Types.ObjectId(),
+//         rating: 'disliked',
+//         tags: ['expensive', 'quiet', 'sushi'],
+//         price: 4
+//     }
+// ];
+
+const createUser = user => {
+    return request(app)
+        .post('/api/auth')
+        .send(user)
+        .then(res => res.body);
+};
+
+beforeEach(() => {
+    return Promise.all(users.map(createUser)).then(userRes => {
+        createdUsers = userRes;
+    });
+});
+
+const getUsers = () => createdUsers;
+
+module.exports = {
+    getUsers
+};
