cac08bb47e5e90dc8839da11c18fa2ee5741831f
Sophie Graham <sophiergraham1@gmail.com>
2018-10-22 12:02:48 -0700

    passing does not add auth token if no auth header provided test

--- test/unit/bearer-token.test.js
+++ test/unit/bearer-token.test.js
@@ -19,4 +19,21 @@ describe('bearer-token middleware', () => {
         expect(called).toBeTruthy();
         expect(req.token).toEqual(token);
     });
+    it('does not add auth token if no auth header provided', () => {
+        const req = {
+            get: () => {
+                return null;
+            }
+        };  
+
+        let called = false;
+        const next = () => {
+            called = true;
+        };
+
+        bearerToken(req, null, next);
+
+        expect(called).toBeTruthy();
+        expect(req.token).toBeUndefined();
+    });
 });
