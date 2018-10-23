b78121de8e85a8938d6a486e76af7512f222902b
Ryan Luras <ryanluras@gmail.com>
2018-10-23 11:48:06 -0700

    Ryanbranch (#9)
    * evening unit test passing, working on mock data
    
    * mocking data for end to end tests
    
    * fixing conflict
    
    * temp post to log
    
    * working on evening
    
    * figured out test issue

--- test/unit/evening.test.js
+++ test/unit/evening.test.js
@@ -18,9 +18,11 @@ describe('Evening model', () => {
     it('fails when no required values are given', () => {
         const evening = new Evening ({});
 
-        const errors = getErrors(evening.validateSync(), 3);
+        const errors = getErrors(evening.validateSync(), 4);
         expect(errors.user_id.properties.message).toEqual('user_id required');
         expect(errors.rating.properties.message).toEqual('rating required');
         expect(errors.logs.properties.message).toEqual('Three Google Place Id\'s required');
+        expect(errors.price.properties.message).toEqual('price required 0-4');
+
     });
 });
