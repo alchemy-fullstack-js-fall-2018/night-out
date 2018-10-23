a55d96212e0153c168b03a2ecaf510003e956a33
Ryan Luras <ryanluras@gmail.com>
2018-10-22 17:43:01 -0700

    Ryanbranch (#8)
    * evening unit test passing, working on mock data
    
    * mocking data for end to end tests
    
    * fixing conflict
    
    * temp post to log

---
+++ test/e2e/logs.test.js
@@ -0,0 +1,30 @@
+const request = require('supertest');
+const app = require('../../lib/app');
+const { getUsers } = require('./mockData');
+
+describe('validates vertical sclices of log posts', () => {
+
+    it('posts a log', () => {
+        const createdUsers = getUsers();
+
+        return request(app)
+            .post('/api/logs')
+            .send({ 
+                place_id: 'ChIJIfBAsjeuEmsRdgu9Pl1Ps48', 
+                user_id: createdUsers[0]._id,
+                rating: 'liked',
+                tags: ['happy hour', 'mexican', 'bar'],
+                price: 2
+            })
+            .then(res => {
+                expect(res.body).toEqual({
+                    _id: expect.any(String),
+                    place_id: 'ChIJIfBAsjeuEmsRdgu9Pl1Ps48',
+                    user_id: createdUsers[0]._id,
+                    rating: 'liked',
+                    tags: ['happy hour', 'mexican', 'bar'],
+                    price: 2
+                });
+            });
+    });
+});
--- test/e2e/mockData.js
+++ test/e2e/mockData.js
@@ -3,7 +3,7 @@ require('../../lib/util/connect')();
 const { dropCollection } = require('../util/db');
 const request = require('supertest');
 const app = require('../../lib/app');
-// const { Types } = require('mongoose');
+const { Types } = require('mongoose');
 
 beforeEach(() => {
     return dropCollection('users');
@@ -11,56 +11,62 @@ beforeEach(() => {
 beforeEach(() => {
     return dropCollection('logs');
 });
-beforeEach(() => {
-    return dropCollection('evenings');
-});
 
 let createdUsers;
-// let createdEvenings;
-// let createdLogs;
+let createdLogs;
 
 let users = [
     {
         name: 'User1',
         email: 'user1@user1.com',
         zipcode: '97202',
+        clearPassword: 'abcd1',
         initialPreferences: ['pref1', 'pref2', 'pref3', 'pref4', 'pref5', 'pref6', 'pref7', 'pref8', 'pref9']
     },
     {
         name: 'User2',
         email: 'user2@user2.com',
         zipcode: '94061',
+        clearPassword: 'abcd2',
         initialPreferences: ['pref1', 'pref2', 'pref3', 'pref4', 'pref5', 'pref6', 'pref7', 'pref8', 'pref9']
     },
     {
         name: 'User3',
         email: 'user3@user3.com',
         zipcode: '97214',
+        clearPassword: 'abcd3',
         initialPreferences: ['pref1', 'pref2', 'pref3', 'pref4', 'pref5', 'pref6', 'pref7', 'pref8', 'pref9']
     }
 ];
 
-// let logs = [
-//     {
-//         place_id: 'ChIJIfBAsjeuEmsRdgu9Pl1Ps48',
-//         user_id: Types.ObjectId(),
-//         rating: 'liked',
-//         tags: ['cheap', 'quiet', 'italian'],
-//         price: 1
-//     },
-//     {
-//         place_id: 'ChIJIfBAsjeuEmsRdgu9Pl1Ps73',
-//         user_id: Types.ObjectId(),
-//         rating: 'disliked',
-//         tags: ['expensive', 'quiet', 'sushi'],
-//         price: 4
-//     }
-// ];
+let logs = [
+    {
+        place_id: 'ChIJIfBAsjeuEmsRdgu9Pl1Ps48',
+        user_id: Types.ObjectId(),
+        rating: 'liked',
+        tags: ['cheap', 'quiet', 'italian'],
+        price: 1
+    },
+    {
+        place_id: 'ChIJIfBAsjeuEmsRdgu9Pl1Ps73',
+        user_id: Types.ObjectId(),
+        rating: 'disliked',
+        tags: ['expensive', 'quiet', 'sushi'],
+        price: 4
+    }
+];
 
 const createUser = user => {
     return request(app)
-        .post('/api/auth')
+        .post('/api/auth/signup')
         .send(user)
+        .then(res => res.body);
+};
+
+const createLog = log => {
+    return request(app)
+        .post('/api/logs')
+        .send(log)
         .then(res => res.body);
 };
 
@@ -70,8 +76,19 @@ beforeEach(() => {
     });
 });
 
+beforeEach(() => {
+    return Promise.all(logs.map(createLog)).then(logRes => {
+        createdLogs = logRes;
+        logs[0].user_id = createdUsers[0]._id;
+        logs[1].user_id = createdUsers[1]._id;
+    });
+});
+
 const getUsers = () => createdUsers;
+const getLogs = () => createdLogs;
 
 module.exports = {
-    getUsers
+    getUsers,
+    getLogs
 };
+
