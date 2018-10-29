569c51f7316d0661108e6d44571b06f611c303a6
Ryan Mehta <ryan@vrtigo.io>
2018-10-29 15:55:26 -0700

    add comments/refactor

--- test/integration/create-logs.test.js
+++ test/integration/create-logs.test.js
@@ -1,7 +1,19 @@
 const { getUsers } = require('../e2e/mockData');
 const createLogs = require('../../lib/util/create-logs');
 
+// Use your mock here so we can do more interesting tests
+jest.mock('../../lib/util/google-map-service');
+
 describe('create logs', () => {
+
+    it('creates logs', () => {
+        const users = getUsers();
+
+        createLogs(users[0], { zipcode: 97206 })
+            .then(logs => {
+                expect(logs).toHaveLength(3);
+            });
+    });
 
     it('creates logs', () => {
         const users = getUsers();
